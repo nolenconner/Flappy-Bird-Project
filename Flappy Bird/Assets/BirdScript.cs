e3e02b60363d4fd098bc95bd660e9ca190e5791e
laRayna <101940870+laRayna@users.noreply.github.com>
2023-01-17 20:46:37 -0600

    game over and platforms
    - game over screen and play again button
    - platforms as a ground and ceiling (bird dies if goes outside of the screen)

--- Flappy Bird/Assets/BirdScript.cs
+++ Flappy Bird/Assets/BirdScript.cs
@@ -6,18 +6,26 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D RigidBody; //access the velocity to go up
     public float Strength; //higher the number, more bouncy the bird is 
+    public LogicScript logic; //calling to logic class
+    public bool isAlive = true; //sees if bird is alive
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {   
-        if (Input.GetKeyDown(KeyCode.Space) == true){
-            RigidBody.velocity = Vector2.up * Strength; //goes up everytime space is clicked
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive){
+            RigidBody.velocity = Vector2.up * Strength; //goes up everytime space is clicked and bird is alive
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){ //dies and game over when collides with object
+        logic.gameOver();
+        isAlive = false;
     }
 }
--- Flappy Bird/Assets/LogicScript.cs
+++ Flappy Bird/Assets/LogicScript.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 
 
 public class LogicScript : MonoBehaviour
 {
-public int playerScore;
-public Text scoreText;
+    public int playerScore;
+    public Text scoreText;
+    public GameObject overScreen;
 
-[ContextMenu("Increase Score")]
-public void addScore(){
-    playerScore = playerScore + 1;
-    scoreText.text = playerScore.ToString();
-}
+
+    [ContextMenu("Increase Score")]
+    public void addScore(){
+        playerScore = playerScore + 1;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver(){
+        overScreen.SetActive(true);
+    }
 }
